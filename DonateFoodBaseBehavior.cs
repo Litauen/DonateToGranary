50ecfd04d7e88de003f364b215d772dff51906d0
Litauen <litauenlitauen@gmail.com>
2023-04-12 13:08:41 +0300

    2.1.0

--- DonateFoodBaseBehavior.cs
+++ DonateFoodBaseBehavior.cs
@@ -2,14 +2,15 @@
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Actions;
 using TaleWorlds.CampaignSystem.GameMenus;
+using TaleWorlds.CampaignSystem.Inventory;
 using TaleWorlds.CampaignSystem.Overlay;
 using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Roster;
 using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.Core;
 using TaleWorlds.Engine;
 using TaleWorlds.Engine.GauntletUI;
 using TaleWorlds.GauntletUI.Data;
-using TaleWorlds.Library;
 using TaleWorlds.Localization;
 using TaleWorlds.ObjectSystem;
 
@@ -22,6 +23,8 @@ namespace DonateToGranary
         private static GauntletMovie? _gauntletMovie;
         private static DTGPopupVM? _popupVM;
 
+        ItemRoster _donateRoster = new();
+
         public override void RegisterEvents()
         {
             CampaignEvents.OnSessionLaunchedEvent.AddNonSerializedListener(this, new Action<CampaignGameStarter>(this.AddGranaryMenuItems));
@@ -33,7 +36,7 @@ namespace DonateToGranary
             int playerGrain = 0;
             int freeSpaceInGranary = 0;
 
-            campaignGameStarter.AddGameMenuOption("town_keep", "granary_keep", "Visit the granary", delegate (MenuCallbackArgs args)
+            campaignGameStarter.AddGameMenuOption("town_keep", "granary_keep", new TextObject("Visit the granary").ToString(), delegate (MenuCallbackArgs args)
             {
                 args.optionLeaveType = GameMenuOption.LeaveType.Submenu;
                 return true;
@@ -85,7 +88,7 @@ namespace DonateToGranary
 
 
 
-            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_keep_add_max", "Add the maximum amount of grain", delegate (MenuCallbackArgs args)
+            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_keep_add_max", new TextObject("Add the maximum amount of grain").ToString(), delegate (MenuCallbackArgs args)
             {
                 if (playerGrain == 0 || freeSpaceInGranary == 0) return false;
                 args.optionLeaveType = GameMenuOption.LeaveType.Ransom;
@@ -96,7 +99,7 @@ namespace DonateToGranary
             }, false, -1, false);
 
             
-            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_keep_add_50", "Add 50 units of grain", delegate (MenuCallbackArgs args)
+            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_keep_add_50", new TextObject("Add 50 units of grain").ToString(), delegate (MenuCallbackArgs args)
             {
 
                 if (playerGrain < 50 || freeSpaceInGranary < 50) return false;
@@ -107,7 +110,7 @@ namespace DonateToGranary
                 this.AddGrain(50);
             }, false, -1, false);
 
-            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_keep_add_100", "Add 100 units of grain", delegate (MenuCallbackArgs args)
+            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_keep_add_100", new TextObject("Add 100 units of grain").ToString(), delegate (MenuCallbackArgs args)
             {
                 if (playerGrain < 100 || freeSpaceInGranary < 100) return false;
                 args.optionLeaveType = GameMenuOption.LeaveType.Ransom;
@@ -117,7 +120,7 @@ namespace DonateToGranary
                 this.AddGrain(100);
             }, false, -1, false);
 
-            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_keep_add_150", "Add 150 units of grain", delegate (MenuCallbackArgs args)
+            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_keep_add_150", new TextObject("Add 150 units of grain").ToString(), delegate (MenuCallbackArgs args)
             {
                 if (playerGrain < 150 || freeSpaceInGranary < 150) return false;
                 args.optionLeaveType = GameMenuOption.LeaveType.Ransom;
@@ -127,7 +130,7 @@ namespace DonateToGranary
                 this.AddGrain(150);
             }, false, -1, false);
 
-            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_keep_add_200", "Add 200 units of grain", delegate (MenuCallbackArgs args)
+            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_keep_add_200", new TextObject("Add 200 units of grain").ToString(), delegate (MenuCallbackArgs args)
             {
                 if (playerGrain < 200 || freeSpaceInGranary < 200) return false;
                 args.optionLeaveType = GameMenuOption.LeaveType.Ransom;
@@ -137,7 +140,7 @@ namespace DonateToGranary
                 this.AddGrain(200);
             }, false, -1, false);
 
-            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_keep_add_300", "Add 300 units of grain", delegate (MenuCallbackArgs args)
+            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_keep_add_300", new TextObject("Add 300 units of grain").ToString(), delegate (MenuCallbackArgs args)
             {
                 if (playerGrain < 300 || freeSpaceInGranary < 300) return false;
                 args.optionLeaveType = GameMenuOption.LeaveType.Ransom;
@@ -146,9 +149,20 @@ namespace DonateToGranary
             {
                 this.AddGrain(300);
             }, false, -1, false);
-            
 
-            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_keep_decline", "Leave", delegate (MenuCallbackArgs args)
+            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_other_foold", new TextObject("Add other type of food").ToString(), delegate (MenuCallbackArgs args)
+            {
+                if (freeSpaceInGranary < 1) return false;
+                args.optionLeaveType = GameMenuOption.LeaveType.Ransom;
+                return true;
+            }, delegate (MenuCallbackArgs args)
+            {
+
+                InventoryManager.OpenScreenAsReceiveItems(_donateRoster, new TextObject("Granary"), new InventoryManager.DoneLogicExtrasDelegate(OnInventoryScreenDone));
+
+            }, false, -1, false);
+
+            campaignGameStarter.AddGameMenuOption("granary_keep_menu", "granary_keep_decline", new TextObject("Leave").ToString(), delegate (MenuCallbackArgs args)
             {
                 args.optionLeaveType = GameMenuOption.LeaveType.Leave;
                 return true;
@@ -167,7 +181,7 @@ namespace DonateToGranary
 
 
             // *** Castle ***
-            campaignGameStarter.AddGameMenuOption("castle", "granary_castle", "Visit the granary", delegate (MenuCallbackArgs args)
+            campaignGameStarter.AddGameMenuOption("castle", "granary_castle", new TextObject("Visit the granary").ToString(), delegate (MenuCallbackArgs args)
             {
                 args.optionLeaveType = GameMenuOption.LeaveType.Submenu;
                 return true;
@@ -218,17 +232,10 @@ namespace DonateToGranary
             transferAmount *= -1;
             PartyBase.MainParty.ItemRoster.AddToCounts(MBObjectManager.Instance.GetObject<ItemObject>("grain"), transferAmount);
 
-            string title = "Grain Transferred";
-            string message = reportAmount.ToString() + " grain have been transferred to " + Settlement.CurrentSettlement.GetName().ToString() + "'s granary.\n";
+            TextObject title = new("Grain Transferred");
+            TextObject message = new(reportAmount.ToString() + " grain have been transferred to " + Settlement.CurrentSettlement.GetName().ToString() + "'s granary.");
 
-            if (Settlement.CurrentSettlement.IsTown)
-            {
-                message += "The local notables were impressed with your generosity.";
-            }
-
-            //InformationManager.ShowInquiry(new InquiryData(title, message, true, false, "OK", "", null, null, ""), true);
-
-            DonateFoodBaseBehavior.CreatePopupVMLayer(title, "", message, "", "donatetogranary4", "Continue");
+            DonateFoodBaseBehavior.CreatePopupVMLayer(title.ToString(), "", message.ToString(), "", "donatetogranary4", new TextObject("Continue").ToString());
 
             SoundEvent.PlaySound2D("event:/ui/panels/panel_clan_open");
 
@@ -262,6 +269,76 @@ namespace DonateToGranary
         }
 
 
+        private void OnInventoryScreenDone()
+        {
+
+            int itemCount = 0;
+            bool otherTypeTransfered = false;
+
+            for (int i = 0; i < _donateRoster.Count; i++)
+            {
+                ItemObject item = _donateRoster.GetItemAtIndex(i);
+                if (item.IsFood)
+                {
+                    itemCount += _donateRoster.GetItemNumber(item);
+                } else
+                {
+                    otherTypeTransfered = true;
+                }
+            }
+
+
+            if (itemCount > 0 || otherTypeTransfered)
+            {
+
+                TextObject title = new("Food Transferred");
+                TextObject message = new(itemCount.ToString() + " unit(s) of food have been transferred to " + Settlement.CurrentSettlement.GetName().ToString() + "'s granary.");
+
+                float creditedAmount = (float)itemCount;
+
+                if (itemCount > 0)
+                {
+                    float foodAmount = Settlement.CurrentSettlement.Town.FoodStocks;
+                    float newFoodAmount = foodAmount + (float)itemCount;
+                    float granaryMaxAmount = (float)Settlement.CurrentSettlement.Town.FoodStocksUpperLimit();
+                    if (newFoodAmount > granaryMaxAmount)
+                    {
+                        newFoodAmount = (float)Settlement.CurrentSettlement.Town.FoodStocksUpperLimit();
+                        otherTypeTransfered = true;
+                        creditedAmount = granaryMaxAmount - foodAmount;
+                    }
+                    Settlement.CurrentSettlement.Town.FoodStocks = newFoodAmount;
+
+                } else
+                {
+                    title = new("Items transferred");
+                    message = new("No food have been transferred...");
+                }
+
+
+                TextObject textOverImage = new("");
+                if (otherTypeTransfered) textOverImage = new("Locals are grateful for additional donations.");
+
+                DonateFoodBaseBehavior.CreatePopupVMLayer(title.ToString(), "", message.ToString(), textOverImage.ToString(), "donatetogranary4", new TextObject("Continue").ToString());
+                SoundEvent.PlaySound2D("event:/ui/panels/panel_clan_open");
+                this.GetCreditForHelping((int)creditedAmount);
+
+                GameMenu.SwitchToMenu("granary_keep_menu");
+
+            }
+
+
+            _donateRoster.Clear();
+
+        }
+
+
+
+
         public override void SyncData(IDataStore dataStore) {}
     }
+
+
 }
+
+
